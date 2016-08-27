af28cdb3cb00ea905fba7e2463451a086e9d0021
Rajat Mhetre <mhetrerajat@yahoo.in>
2016-08-27 19:23:43 +0530

    api is documented

--- api/app/handler.py
+++ api/app/handler.py
@@ -1,143 +1,274 @@
+"""
+	This modules has functions bind to particular route. Making request
+	for that route will excute those functions and returns appropriate results.
+	Route mentioned for function is just half URI i.e. it exculdes hostname.
+
+	Attributes:
+		LAT_LNG_RANGE (list) : List of all possible values for latitude and longitude.
+
+"""
+from datetime import datetime
 from flask import jsonify, request
 from app import db, app, Serializer, Car, Customer
-import json
-from datetime import datetime
 
 
 LAT_LNG_RANGE = list(range(-180, 181))
 
 
-def getBool(input_string):
-	if type(input_string) is str:
-		if input_string.lower() == 'false':
-			return False
-		else:
-			return True
-	else:
-		return False
+def get_bool(input_string):
+	"""
+		This function takes string and gives its corresponding boolean value.
+		If given input is false then it returns False
 
+		Return: Boolean
+
+		Args:
+			input_string (str) : Any string 
+	"""
+    if isinstance(input_string, str):
+        if input_string.lower() == 'false':
+            return False
+        else:
+            return True
+    else:
+        return False
 
 
 @app.route("/")
 def hello():
-	return jsonify({ 'message' : 'Welcome to fuber.'})
+	"""
+		Just sample function bound with route '/'.
+
+		Return:
+			{
+				'message' : 'Welcome to fuber.'
+			}
+	"""
+    return jsonify({'message': 'Welcome to fuber.'})
 
 
 @app.route("/car/<int:car_id>", methods=['GET'])
 def get_car_details(car_id):
-	car = Car.query.filter_by(id=car_id).first()
-	return jsonify({ 'result' : car.serialize()})
+	"""
+		Gives details about particular car. Function bound with route '/car/<car_id>'
+		and only GET method is allowed.
+
+		Args:
+			car_id (int) : its the unique id of car
+
+		Return:
+			Instance of Car class serialized in json.
+	"""
+    car = Car.query.filter_by(id=car_id).first()
+    return jsonify({'result': car.serialize()})
 
 
 @app.route("/car", methods=['GET'])
 def get_cars():
-	available_status = request.args.get('available_status')
-	is_pink = request.args.get('is_pink')
-	if available_status and is_pink:
-		# show available and pink cars
-		car_list = Car.query.filter_by(available_status=True, is_pink=True).all()
-	elif is_pink:
-		# show pink cars
-		car_list = Car.query.filter_by(is_pink=True).all()
-	elif available_status:
-		# show available cars
-		car_list = Car.query.filter_by(available_status=True).all()
-	else:
-		# show all cars
-		car_list = Car.query.all()
-	return jsonify({ 'result' : Serializer.serialize_list(car_list)})
+	"""
+		Gives list of cars according filter arguments.
+		Only GET method is allowed with route '/car'
+
+		Args:
+			available_status (bool) : if True, show cars available for booking
+										and on False, show cars which are booked [Optional]
+			is_pink (bool) : If true, show pink cars else on False, show cars
+									which are not pink. [Optional]
+
+		Return:
+			List of cars matched with filter arguments
+	"""
+    available_status = request.args.get('available_status')
+    is_pink = request.args.get('is_pink')
+    if available_status and is_pink:
+            # show available and pink cars
+        car_list = Car.query.filter_by(
+            available_status=True, is_pink=True).all()
+    elif is_pink:
+            # show pink cars
+        car_list = Car.query.filter_by(is_pink=True).all()
+    elif available_status:
+            # show available cars
+        car_list = Car.query.filter_by(available_status=True).all()
+    else:
+            # show all cars
+        car_list = Car.query.all()
+    return jsonify({'result': Serializer.serialize_list(car_list)})
 
 
 @app.route("/customer", methods=['GET'])
 def get_customers():
-	customer_list = Customer.query.all()
-	return jsonify(dict(result=Serializer.serialize_list(customer_list)))
+	"""
+		Give list of customers. All customers who booked previously
+		and the ones who booked it currently. 
+		Only GET method is allowed with route '/customer'
+
+		Return:
+			List of customers.
+	"""
+    customer_list = Customer.query.all()
+    return jsonify(dict(result=Serializer.serialize_list(customer_list)))
 
 
 @app.route("/car", methods=['POST'])
 def add_car():
-	response = None
-	try:
-		params = dict()
-		params.update({ 'latitude' : request.form.get(['latitude'])})
-		params.update({ 'longitude' : request.form.get(['longitude'])})
-		params.update({ 'is_pink' : getBool(request.form.get(['is_pink']))})
+	"""
+		Insert cars into database.
+		Only POST method is allowed with route '/car'
 
-		if round(params.get('latitude')) in LAT_LNG_RANGE and round(params.get('longitude')) in LAT_LNG_RANGE:
-			car = Car(params.get('latitude'), params.get('longitude'), params.get('is_pink'))
-			db.session.add(car)
-			db.session.commit()
-			response = jsonify(dict(result=params))
-		else:
-			response = jsonify(dict(result="Please provide valid latitude/longitude."))
-	except ValueError as e:
-		response = jsonify({ 'result' : 'Please provide valid latitude/longitude.'})
-	except Exception as e:
-		response = jsonify(dict(result="Oops! Something went wrong. Please try again."))
-	return response
+		Args:
+			latitude (float) : latitude of current location
+			longitude (float) : longitude of current location
+			is_pink (bool) : whether the car is pink, if it is pink then True
+							else, set to False [optional]
+
+	"""
+    response = None
+    try:
+        params = dict()
+        params.update({'latitude': request.form.get(['latitude'])})
+        params.update({'longitude': request.form.get(['longitude'])})
+        params.update({'is_pink': get_bool(request.form.get(['is_pink']))})
+
+        if round(params.get('latitude')) in LAT_LNG_RANGE and round(
+                params.get('longitude')) in LAT_LNG_RANGE:
+            car = Car(
+                params.get('latitude'),
+                params.get('longitude'),
+                params.get('is_pink'))
+            db.session.add(car)
+            db.session.commit()
+            response = jsonify(dict(result=params))
+        else:
+            response = jsonify(
+                dict(result="Please provide valid latitude/longitude."))
+    except ValueError as e:
+        response = jsonify(
+            {'result': 'Please provide valid latitude/longitude.'})
+    except Exception as e:
+        response = jsonify(
+            dict(result="Oops! Something went wrong. Please try again."))
+    return response
 
 
 @app.route('/customer/<int:customer_id>/complete', methods=['POST'])
 def finish_journey(customer_id):
-	response = None
-	try:
-		params = dict()
-		params.update({ 'latitude' : float(request.form.get('latitude', None))})
-		params.update({ 'longitude' : float(request.form.get('longitude', None))})
-		params.update({ 'customer_id' : int(customer_id)})
+	"""
+		Finish car trip for particular customer.
+		Only POST method is allowed with route /customer/<customer_id>/complete
 
-		if round(params.get('latitude')) in LAT_LNG_RANGE and round(params.get('longitude')) in LAT_LNG_RANGE:
-			booking_customer = Customer.query.filter_by(id=params.get('customer_id')).first()
-			booking_car = Car.query.filter_by(id=booking_customer.car_id).first()
-			completed_on = datetime.now()
-			booking_amount = Customer.set_amount(booking_customer.booked_on, completed_on, booking_car.is_pink)
-			Customer.query.filter_by(id=params.get('customer_id')).update(dict(completed_on=completed_on, amount=booking_amount))
-			Car.query.filter_by(id=booking_customer.car_id).update(dict(available_status=True, latitude=params.get('latitude'), longitude=params.get('longitude')))
-			db.session.commit()
-			
-			response = jsonify(dict(result="Journey completed successfully."))
-		else:
-			response = jsonify({'result' : "Please provide valid latitude and longitude."})
-	except ValueError as e:
-		response = jsonify(dict(result="Please provide valid latitude/longitude."))
-	except AttributeError as e:
-		response = jsonify(dict(result="Oops! Customer with given id does not exists. Please check customer_id."))
-	except Exception as e:
-		response = jsonify(dict(result="Oops! Something went wrong. Please try again."))
-	return response
+		Args:
+			customer_id (int) : unique id of the customer
+			latitude (float) : latitude of current location
+			longitude (float) : longitude of current location
+	"""
+    response = None
+    try:
+        params = dict()
+        params.update({'latitude': float(request.form.get('latitude', None))})
+        params.update(
+            {'longitude': float(request.form.get('longitude', None))})
+        params.update({'customer_id': int(customer_id)})
+
+        if round(params.get('latitude')) in LAT_LNG_RANGE and round(
+                params.get('longitude')) in LAT_LNG_RANGE:
+
+        	# customer with given customer_id
+            booking_customer = Customer.query.filter_by(
+                id=params.get('customer_id')).first()
+
+            # car booked by given customer
+            booking_car = Car.query.filter_by(
+                id=booking_customer.car_id).first()
+            completed_on = datetime.now()
+
+            # calculate amount to be paid by customer after completion of trip
+            booking_amount = Customer.set_amount(
+                booking_customer.booked_on, completed_on, booking_car.is_pink)
+            Customer.query.filter_by(id=params.get('customer_id')).update(
+                dict(completed_on=completed_on, amount=booking_amount))
+
+            # update new location of car
+            Car.query.filter_by(
+                id=booking_customer.car_id).update(
+                    dict(available_status=True, latitude=params.get('latitude'),
+                	        longitude=params.get('longitude')))
+            db.session.commit()
+
+            response = jsonify(dict(result="Journey completed successfully."))
+        else:
+            response = jsonify(
+                {'result': "Please provide valid latitude and longitude."})
+    except ValueError as e:
+        response = jsonify(
+            dict(result="Please provide valid latitude/longitude."))
+    except AttributeError as e:
+        response = jsonify(
+            dict(
+                result="Oops! Customer with given id does not exists. Please check customer_id."))
+    except Exception as e:
+        response = jsonify(
+            dict(result="Oops! Something went wrong. Please try again."))
+    return response
 
 
 @app.route('/customer/book', methods=['POST'])
 def book_car():
-	response = None
-	try:
-		params = dict()
-		params.update({ 'latitude' : float(request.form.get('latitude'))})
-		params.update({ 'longitude' : float(request.form.get('longitude'))})
-		params.update({ 'is_pink' : getBool(request.form.get('is_pink'))})
-		params.update({ 'booked_on' : datetime.now()})
+	"""
+		Book car
+		Only POST method is allowed with route /customer/book
 
-		if round(params.get('latitude')) in LAT_LNG_RANGE and round(params.get('longitude')) in LAT_LNG_RANGE:
-			available_cars = Car.query.filter_by(is_pink=params.get('is_pink'), available_status=True).all()
+		Args:
+			latitude (float) : current location (latitude) of customer
+			longitude (float) : current location (longitude) of customer
+			is_pink (bool) : does customer want pink car, true if want else False [Optional]
+			booked_on (DateTime) : booking time [AutoCalculate]
+	"""
+    response = None
+    try:
+        params = dict()
+        params.update({'latitude': float(request.form.get('latitude'))})
+        params.update({'longitude': float(request.form.get('longitude'))})
+        params.update({'is_pink': get_bool(request.form.get('is_pink'))})
+        params.update({'booked_on': datetime.now()})
 
-			if available_cars:
-				distance = {}
-				for car in available_cars:
-					car_distance = Car.distance_travelled(params.get('latitude'), params.get('longitude'), car)
-					distance.update({ car.id : car_distance})
-				booking_car_id = sorted(distance.items(), key=lambda x: x[1])[0][0]
-				customer = Customer(booking_car_id)
-				db.session.add(customer)
-				Car.query.filter_by(id=booking_car_id).update(dict(available_status=False))
-				db.session.commit()
-				response = jsonify({ 'result' : Serializer.serialize_list(available_cars)})
-			else:
-				response = jsonify({ 'result' : "NO_CARS_AVAILABLE"})
-		else:
-			response = jsonify({'result' : "Please provide valid latitude and longitude."})
-	except ValueError as e:
-		response = jsonify({'result' : "Please provide valid latitude/longitude."})
-	except Exception as e:
-		response = jsonify({'result' : "Oops! Something went wrong. Please try again."})
+        if round(params.get('latitude')) in LAT_LNG_RANGE and round(
+                params.get('longitude')) in LAT_LNG_RANGE:
 
-	return response
+        	# check for cars as per customer's preference
+            available_cars = Car.query.filter_by(
+                is_pink=params.get('is_pink'), available_status=True).all()
+
+            if available_cars:
+            	# calculate distance between car's location and customer's location
+                distance = {}
+                for car in available_cars:
+                    car_distance = Car.distance_travelled(
+                        params.get('latitude'), params.get('longitude'), car)
+                    distance.update({car.id: car_distance})
+                # get id of car which is nearest to customer
+                booking_car_id = sorted(
+                    distance.items(), key=lambda x: x[1])[0][0]
+                # alot that car to customer
+                customer = Customer(booking_car_id)
+                db.session.add(customer)
+                Car.query.filter_by(
+                    id=booking_car_id).update(
+                        dict(
+                            available_status=False))
+                db.session.commit()
+                response = jsonify(
+                    {'result': Serializer.serialize_list(available_cars)})
+            else:
+                response = jsonify({'result': "NO_CARS_AVAILABLE"})
+        else:
+            response = jsonify(
+                {'result': "Please provide valid latitude and longitude."})
+    except ValueError as e:
+        response = jsonify(
+            {'result': "Please provide valid latitude/longitude."})
+    except Exception as e:
+        response = jsonify(
+            {'result': "Oops! Something went wrong. Please try again."})
+
+    return response
--- api/app/models/car.py
+++ api/app/models/car.py
@@ -1,23 +1,63 @@
+"""
+	This module is model of Car. It has attributes id, is_pink, latitude,
+	longitude and available_status.
+	It maps with table car in database.
+"""
+import math
 from app import db
 from ..serializer import Serializer
-import math
 
 class Car(db.Model, Serializer):
+	"""
+		Car class inherits from two classes db.Model and Serializer.
+		It maps attributes defined in class with the columns in car table.
+		Every instance of Car class represents one row in car table.
+
+		Note:
+        	Do not include the `self` parameter in the ``Args`` section.
+
+        Attributes:
+        	id (int): Unique id to represent car.
+        	is_pink (bool): Its true, if car is pink else its false.
+        	latitude (float) : Location of car
+        	longitude (float) : Location of car
+        	available_status (bool) : Its true, if car is available for booking else, its set to false.
+	"""
 	id = db.Column(db.Integer, primary_key=True)
 	is_pink = db.Column(db.Boolean, default=False)
 	latitude = db.Column(db.Float(precision=64))
 	longitude = db.Column(db.Float(precision=64))
 	available_status = db.Column(db.Boolean, default=True)
 
-	def __init__(self, latitude, longitude, is_pink=False):
-		self.latitude = latitude
-		self.longitude = longitude
-		self.is_pink = is_pink
+    def __init__(self, latitude, longitude, is_pink=False):
+    	"""
+    		__init__ method
+    		Note:
+        		Do not include the `self` parameter in the ``Args`` section.
 
-	def __json__(self):
-		return ['id', 'is_pink', 'latitude', 'longitude', 'available_status']
+    		Args:
+        		is_pink (bool): Its true, if car is pink else its false. [Optional]
+        		latitude (float) : Location of car
+        		longitude (float) : Location of car
+    	"""
+        self.latitude = latitude
+        self.longitude = longitude
+        self.is_pink = is_pink
+
+    def __json__(self):
+    	"""
+    		Serialize json 
+    	"""
+        return ['id', 'is_pink', 'latitude', 'longitude', 'available_status']
 
 
-	@staticmethod
-	def distance_travelled(current_latitude, current_longitude, original_car_location):
-		return math.sqrt((current_latitude-original_car_location.latitude)**2 + (current_longitude-original_car_location.longitude)**2)
+    @staticmethod
+    def distance_travelled(current_latitude, current_longitude, original_car_location):
+    	"""
+    		Calulates distance between to current location and old location.
+    		Args:
+        		current_latitude (float): Current latitude of car
+        		current_longitude (float) : Current longitude of car
+        		original_car_location (Car) : Instance of Car class. Gives just previous location of car i.e location at the time of booking.
+    	"""
+        return math.sqrt((current_latitude-original_car_location.latitude)**2 + (current_longitude-original_car_location.longitude)**2)
